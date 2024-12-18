1121f83529cff3e4e8742d5f665ddb51fd170bac
Irfan140 <irfanmehmud140@gmail.com>
2024-12-18 14:33:19 +0530

    learned how to use code forces

---
+++ 01_basic_testing/02_contest_2044_problem_A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; // no. of test cases
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        cout << n-1 <<endl;
+    }
+}
